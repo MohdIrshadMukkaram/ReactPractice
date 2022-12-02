e3503fb5aae505442d05db479eac5431e57386e2
irshad <mohd.mukkaram120@gmail.com>
2022-12-01 21:13:34 -0500

    Added css and code organization

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,13 +1,15 @@
+import "./ExpenseDate.css";
+
 function ExpenseDate(props) {
     const date = props.date.toLocaleString('en-US', { month: 'long'});
     const day = props.date.toLocaleString('en-US', { day: '2-digit'});
     const year = props.date.getFullYear();
 
     return (
-        <div>
-            <p>{date}</p>
-            <p>{day}</p>
-            <p>{year}</p>
+        <div className="expense-date">
+            <div className="expense-date__date">{date}</div>
+            <div className="expense-date__day">{day}</div>
+            <div className="expense-date__year">{year}</div>
         </div>
     );
 }
